3cbad75db5951848bafc57e0c2729a89652b8feb
karthik18 <karthikmaddala18@gmail.com>
2018-03-25 22:56:47 -0700

    Add files via upload

---
+++ ReadWeatherData.java
@@ -0,0 +1,10 @@
+package com.att.ncdc.analysis.sourcedata;
+
+public class ReadWeatherData {
+	String sourceDirectoryPath = "C:/Data/NCDCSourceData/010010-99999-2012";
+	
+	public static void main(String[] args){
+		
+	}
+
+}
